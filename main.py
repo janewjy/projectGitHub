b1923c80d1838cc8bfbc9438ff97b279c9808d55
Jingya <janewjy87@gmail.com>
2015-05-22 13:30:33 -0400

    shopping list using jinja2

--- main.py
+++ main.py
@@ -42,14 +42,14 @@ class MainPage(Handler):
         items = self.request.get_all("food")
         self.render("shopping_list.html", items = items)
         
-# class FizzBuzzHandler(Handler):
-#     def get(self):
-#         n = self.request.get("n",0)
-#         if n:
-#             n = int(n)
-#         self.render("fizzbuzz.html",n=n)
+class FizzBuzzHandler(Handler):
+    def get(self):
+        n = self.request.get("n",0)
+        if n:
+            n = int(n)
+        self.render("fizzbuzz.html",n=n)
 		
 
 app = webapp2.WSGIApplication([('/', MainPage),
-    # ('/fizzbuzz',FizzBuzzHandler),
+    ('/fizzbuzz',FizzBuzzHandler),
     ], debug=True)
